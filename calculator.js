cdd6f088e667b2b0b92c9311a6334b9e959d396f
masaki_okajima <o.k.j.w.s.i.0123@gmail.com>
2023-05-14 13:39:19 +0900

    update

--- calculator.js
+++ calculator.js
@@ -1,13 +1,37 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res)
-{
+//ブラウザからのgetリクエストに対して
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function ()
-{
+//ブラウザからのpostメソッドに対して
+app.post("/", function (req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var operator = req.body.operator;
+    switch (operator) {
+        case "1":
+            var result = num1 + num2;
+            break;
+        case "2":
+            var result = num1 - num2;
+            break;
+        case "3":
+            var result = num1 * num2;
+            console.log(result);
+            break;
+        case "4":
+            var result = Math.round(num1 / num2);
+    }
+    // console.log(result);
+    res.send("答えは" + result + "です。");
+});
+
+app.listen(3000, function () {
     console.log("server is running...");
 });
